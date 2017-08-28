9bd568e90da80aa088f002e2607fdb129bde2fc6
Franklin Sarkett <franklin.sarkett@gmail.com>
2017-08-28 10:43:21 -0500

    Version bump 0.4.2

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 import os
 import sys
 
+version = '0.4.2'
+
 try:
     from setuptools import setup
 except ImportError:
@@ -16,14 +18,6 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
-
-
-with open("smartystreets/__init__.py", "r") as module_file:
-    for line in module_file:
-        if line.startswith("__version__"):
-            version_string = line.split("=")[1]
-            version = version_string.strip().replace("'", "")
-
 
 requirements = [
     'requests>=2.0.0',
@@ -45,12 +39,8 @@ setup(
     version=version,
     description='A wrapper for the SmartyStreets address validation and geolocation API"',
     long_description=readme + '\n\n' + history,
-    author='Ben Lopatin',
-    author_email='ben@wellfire.co',
-    url='https://github.com/bennylope/smartystreets.py',
-    packages=[
-        'smartystreets',
-    ],
+    url='https://github.com/audantic/smartystreets.py',
+    packages=['smartystreets'],
     package_dir={'smartystreets':
                  'smartystreets'},
     include_package_data=True,
@@ -58,17 +48,6 @@ setup(
     license="BSD",
     zip_safe=False,
     keywords='smarty streets',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
     test_suite='tests',
     tests_require=test_requirements,
 )
--- smartystreets/__init__.py
+++ smartystreets/__init__.py
@@ -1,8 +1,1 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Ben Lopatin'
-__email__ = 'ben@wellfire.co'
-__version__ = '0.4.1'
-
-
-from .client import Client  # noqa
+from .client import Client
